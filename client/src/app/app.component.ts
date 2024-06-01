e308345205584a0b37ca982130c42596d607e6ed
bolanosfra <bolanos.dean@iisgalvanimi.edu.it>
2024-06-01 11:35:50 +0000

    mongo

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data: any;
+  constructor(private http: HttpClient)
+  {
+    this.http.get("https://5000-bolanosfra-progettodock-0h5x2udkls7.ws-eu114.gitpod.io/simple_json")
+    .subscribe(
+      (data) => this.data = data
+    )
+  }
 }
